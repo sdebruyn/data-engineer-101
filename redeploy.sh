a51cbdf48d7d301df741820416c436603ec9a8aa
Sam Debruyn <sam@debruyn.dev>
2020-05-20 20:42:39 +0200

    f... this, let's go with netlify raspberry pi sdcard got corrupted once again

--- redeploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git pull
-git submodule update --init --recursive
-hugo
-find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec gzip -v -k -f --best {} \;
